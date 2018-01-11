d5663b337476b99053620f27e690edefc258af42
Charles Shin <KCfamily@Charles-MacBook-Pro.local>
2018-01-11 14:28:47 -0800

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,14 +1,30 @@
 
-console.log("Up and running!");
 
 
-//card names
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour =  "king";
+
+//arrays unit 8
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+
+var checkForMatch = function() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+console.log("You found a match!");
+} else {
+console.log("Sorry, try again.");
+}
+}
+
+//unit 9
+var flipCard = function(cardId){
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+if (cardsInPlay.length === 2) {
+checkForMatch();
+}
+}
+
+flipCard(0);
+flipCard(2);
 
 
-//Logs
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+

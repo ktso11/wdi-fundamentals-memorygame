8f1e3165d1ba84c6c559bd9e0947f4c90f11f964
Charles Shin <KCfamily@Charles-MacBook-Pro.local>
2018-01-10 16:21:06 -0800

    created game logic

---
+++ js/main.js
@@ -0,0 +1,14 @@
+
+console.log("Up and running!");
+
+
+//card names
+var cardOne = "queen";
+var cardTwo = "king";
+var cardThree = "queen";
+var cardFour =  "king";
+
+
+//Logs
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardTwo);

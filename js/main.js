ded1348f9d911cfe8433704add4308d1597bd860
Katie YK So <KCfamily@Charles-MacBook-Pro.local>
2018-01-17 22:40:28 -0800

    created game logic unit 10

--- js/main.js
+++ js/main.js
@@ -2,25 +2,54 @@
 
 
 
-//arrays unit 8
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+];
+
+
+
+
+
+
 var cardsInPlay = [];
 
 var checkForMatch = function() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-console.log("You found a match!");
-} else {
-console.log("Sorry, try again.");
-}
+		console.log("You found a match!");
+	} else {
+		console.log("Sorry, try again.");
+	}
 }
 
-//unit 9
 var flipCard = function(cardId){
-	console.log("User flipped " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
-if (cardsInPlay.length === 2) {
-checkForMatch();
-}
+	console.log("User flipped " + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
+
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
+
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
+	}
 }
 
 flipCard(0);
